afca3469bbc3e0808ce5aaada68ebd80d00f06d1
Ruben Menke <rum@bankingcircle.com>
2020-11-04 23:39:00 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 
 import setuptools
 from distutils.core import setup
-
+from neographviz.__init__ import __version__
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
     name='neographviz',
-    version='0.3',
+    version=__version__,
     description='Graph display package for py2neo graphs forked from https://github.com/nicolewhite/neo4j-jupyter',
     long_description = long_description,
     long_description_content_type="text/markdown",
@@ -16,6 +16,6 @@ setup(
     url='https://github.com/icfly2/neographviz',
     packages=['neographviz'],
     package_data={'neographviz': ['templates/*.html']},
-    install_requires = ['py2neo>=2020.0.0', 'IPython', 'jinja2',]
+    install_requires = ['py2neo>=2020.0.0', 'IPython', 'jinja2',],
     extras_require={'app': ['waitress==1.4.3', "flask>=1.1.1"]},
 )
